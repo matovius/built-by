097d1e5b3d952a1714c9e1e13c65d0e2b3cdfea8
Martin Matovu <matovius@proton.me>
2024-06-03 20:33:17 +0300

    Overhaul header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,50 +1,83 @@
 <script lang="ts">
-  /**
-   * Describes whether the page is loaded
-   */
-  export let isPageLoaded: boolean = false;
-
-  /**
-   * The header element
-   */
-  let Header: HTMLElement;
-
-  $: if (isPageLoaded && Header) {
-    Header.style.transform = 'scale(50%)';
-    Header.style.opacity = '0.2';
-  }
-
-  $: if (!isPageLoaded && Header) {
-    Header.style.removeProperty('transform');
-    Header.style.removeProperty('opacity');
-  }
+	import Logo from "./Logo.svelte";
 </script>
 
-<header class="header" bind:this={Header}>
-  <h1 class="heading">
-    <span>Built</span>
-    <br />
-    <span>By</span>
-  </h1>
+<header class="header">
+  <div class="container">
+    <div class="logo-wrapper">
+      <Logo />
+    </div>
+
+    <nav class="nav">
+      <a href="/about" class="link">About</a>
+    </nav>
+  </div>
 </header>
 
 <style>
 	.header {
 		width: 100%;
-		padding-inline: 20px;
-		padding-block: 20px;
-		isolation: isolate;
-    transform-origin: 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+		padding-inline: 30px;
+    background: hsl(var(--clr-burnt-sienna));
+    position: sticky;
+    top: 0;
+    z-index: 10;
 
-		& > .heading {
-			font-size: 90px;
-			font-weight: 900;
-			line-height: 1;
-			color: hsl(var(--clr-black-bean));
+    & > .container {
+      width: 100%;
+      max-width: 1200px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding-block: 30px;
 
-			@media screen and (min-width: 900px) {
-				font-size: 180px;
-			}
-		}
+      & > .logo-wrapper {
+        width: 60px;
+        height: 60px;
+        aspect-ratio: 1;
+      }
+
+      & > .nav {
+        font-size: 20px;
+        font-weight: 600;
+        
+        & > .link {
+          isolation: isolate;
+          line-height: 1.5;
+          color: hsl(var(--clr-black-bean));
+          text-decoration: none;
+          padding-inline: 10px;
+          padding-block: 5px;
+          outline: 2px dashed transparent;
+          outline-offset: 4px;
+          position: relative;
+
+          &::before {
+            content: "";
+            width: 100%;
+            height: 2px;
+            background: hsl(var(--clr-black-bean), 0.2);
+            position: absolute;
+            bottom: 0;
+            inset-inline: 0;
+            z-index: -1;
+          }
+
+          &:is(:hover, :focus) {
+            color: hsl(var(--clr-pale-dogwood));
+            &::before {
+              height: 100%;
+              background: hsl(var(--clr-black-bean));
+            }
+          }
+          &:focus-visible {
+            outline-color: hsl(var(--clr-pale-dogwood));
+          }
+        }
+      }
+    }
 	}
 </style>
