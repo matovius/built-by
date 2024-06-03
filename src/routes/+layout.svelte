1d9f38e4b828477e4d4b9a6c53f3a74f76bf7d96
Martin Matovu <matovius@proton.me>
2024-06-03 20:37:34 +0300

    Add styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,21 @@
 <slot />
 
 <style>
+	.app {
+		width: 100%;
+		height: 100dvh;
+		display: flex;
+		flex-direction: column;
+		overflow-y: auto;
+	}
+
+	.main {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: flex-start;
+		padding-inline: 30px;
+		padding-block: 90px;
+	}
 </style>
